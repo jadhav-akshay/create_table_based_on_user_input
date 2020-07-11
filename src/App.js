2187a9a8c4f0ac4f6968d012e903947e6f880dce
Akshay Jadhav <60853608+Akshay29AJ@users.noreply.github.com>
2020-07-11 18:42:17 +0530

    Add files via upload
    Provide   name and age app will create dynamic table. If name is already exist. it will display error message.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Page1 from './Page1';
+
+function App() {
+  return (
+    <div className="App">
+      <Page1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page1.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import './Page1.css';
+
+
+class Page1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: 0,
+      name:'',
+      isToggle:false,
+      data:[]
+    } ;
+  }
+  saveName(e){
+    this.setState({
+      name: e.target.value
+    });
+  }
+  saveAge(e){
+    this.setState({
+      age: parseInt(e.target.value)
+    });
+  }
+  add(e){
+    this.setState({
+      age: this.state.age+1
+    });
+  }
+  minus(e){
+    if(this.state.age!==0) {
+      this.setState({
+        age: this.state.age-1
+      });
+    }
+  }
+  handleExist(index) {
+    let temp3=this.state.data;
+    for(let i=0;i<temp3.length;i++){
+      if(temp3[i].name===index){
+        return false;
+      }
+    }
+    return true;
+  }
+  insertData(){
+    let temp1={
+      "name":this.state.name,
+      "age":this.state.age
+    };
+    let temp2=this.state.data;
+    temp2.push(temp1);
+    this.setState({ data: temp2});
+    document.getElementById("nameId").value="";
+    this.setState({ age: 0});
+    
+  }
+  submitClick(){
+    this.handleExist(this.state.name) === true
+    ? this.insertData()
+    : window.confirm('This name is already present try other')
+
+  }
+
+  renderTableData() {
+    return this.state.data.map((item, index) => {
+       const {name, age } = item;
+       return (
+          <tr key={name}>
+             <td>{name}</td>
+             <td>{age}</td>
+          </tr>
+       )
+    })
+ }
+ renderTableHeader() {
+  let header = Object.keys(this.state.data[0])
+  return header.map((key, index) => {
+    return <th key={index}>{key.toUpperCase()}</th>
+  })
+ }
+  render() {
+   return (
+    <div style={{"align":"center","paddingTop":100}}>
+      Name<input type="text" id="nameId" onChange={(e)=>this.saveName(e)} />
+      <br/>
+      <button onClick={(e1)=>this.add(e1)}>+</button>
+       Age <input disabled type="number" value={this.state.age}onChange={(e)=>this.saveAge(e)}/>
+      <button onClick={(e1)=>this.minus(e1)}>-</button>
+      <br/>
+      <button disabled={this.state.name===''&this.state.age===0 ? true :false} onClick={(e1)=>this.submitClick(e1)}>Submit</button>
+      {
+        this.state.data.length>0 ?
+          <table id='students'>
+            <tbody>
+              <tr>
+              {this.renderTableHeader()}
+              </tr>
+              {this.renderTableData()}
+            </tbody>
+          </table>
+        :""
+      }
+    </div>
+   );
+  }
+}
+
+export default Page1;
